527f0ef2fd3ec6d733681ebdec2382e6bfeaa729
AgathePons <ponsagathe37@gmail.com>
2022-04-13 16:20:52 +0200

    css quickshop fix when not logged in, but to rework

--- src/components/QuickShop/index.js
+++ src/components/QuickShop/index.js
@@ -21,7 +21,7 @@ export default function QuickShop({ visible, handleClick }) {
       <div className="quickshop" onClick={handleClick}>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M416 352c-8.188 0-16.38-3.125-22.62-9.375L224 173.3l-169.4 169.4c-12.5 12.5-32.75 12.5-45.25 0s-12.5-32.75 0-45.25l192-192c12.5-12.5 32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25C432.4 348.9 424.2 352 416 352z" /></svg>
       </div>
-      <div className={visible ? 'quickshop__block snaps-inline' : 'quickshop__block quickshop-closed'}>
+      <div className="quickshop__block snaps-inline">
         {
           clickFlat[0].length !== 0
             ? clickFlat[0].map((e) => <QuickShopItem key={e.id} id={e.id} cost={e.starting_cost} nextCost={e.next_cost} />)
